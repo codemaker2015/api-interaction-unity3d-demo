d0fe18fd4df8712b76e7acebb2af845f0c2b99dd
Vishnu Sivan <codemaker2015@gmail.com>
2021-07-06 13:01:59 +0530

    Initial commit

---
+++ Assets/Scripts/ApiController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using SimpleJSON;
+
+public class ApiController : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        string url = "https://retrofit-backend-demo.herokuapp.com/book";
+        StartCoroutine(GetBooksUsingWWW(url));
+        StartCoroutine(GetBooksUsingUnityWebRequest(url));
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    IEnumerator GetBooksUsingWWW(string url) {
+        using (WWW www = new WWW(url)){
+            yield return www;
+            Debug.Log(www.text);
+            JSONNode jsonNode = JSON.Parse(www.text);
+            string title = jsonNode[0]["title"].ToString();
+            Debug.Log("Title: " + title);
+        }
+    }
+
+    IEnumerator GetBooksUsingUnityWebRequest(string url) {
+        UnityWebRequest www = UnityWebRequest.Get(url);
+        yield return www.SendWebRequest();
+ 
+        if(www.isNetworkError || www.isHttpError) {
+            Debug.Log(www.error);
+        }
+        else {
+            Debug.Log(www.downloadHandler.text);
+            JSONNode jsonNode = JSON.Parse(www.downloadHandler.text);
+            string title = jsonNode[0]["title"].ToString();
+            Debug.Log("Title: " + title);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,48 @@
+# API Demo Unity 3D
+
+Example project for calling apis using WWW and UnityWebRequest classes.
+
+## Code
+
+```cs
+
+    void Start() {
+        string url = "https://retrofit-backend-demo.herokuapp.com/book";
+        StartCoroutine(GetBooksUsingWWW(url));
+        StartCoroutine(GetBooksUsingUnityWebRequest(url));
+    }
+    
+    IEnumerator GetBooksUsingWWW(string url) {
+        using (WWW www = new WWW(url)){
+            yield return www;
+            Debug.Log(www.text);
+            JSONNode jsonNode = JSON.Parse(www.text);
+            string title = jsonNode[0]["title"].ToString();
+            Debug.Log("Title: " + title);
+        }
+    }
+
+    IEnumerator GetBooksUsingUnityWebRequest(string url) {
+        UnityWebRequest www = UnityWebRequest.Get(url);
+        yield return www.SendWebRequest();
+ 
+        if(www.isNetworkError || www.isHttpError) {
+            Debug.Log(www.error);
+        }
+        else {
+            Debug.Log(www.downloadHandler.text);
+            JSONNode jsonNode = JSON.Parse(www.downloadHandler.text);
+            string title = jsonNode[0]["title"].ToString();
+            Debug.Log("Title: " + title);
+        }
+    }
+
+```
+
+## Screenshots
+![Screenshot](Screenshots/Screenshot.png)
+
+## Requirements
+
+* Unity 2019.4.17f1
+* SimpleJSON 
